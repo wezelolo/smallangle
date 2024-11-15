5fc7b769a7de941904e73b3ba73b7b16e0cbaf19
wezelolo <wasiel.kraakman@gmail.com>
2024-11-15 15:52:13 +0100

    updated the docstrings so it doesn't have te args

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -16,16 +16,12 @@ def cmd_group():
     "--number",
     type=int,
     default=10,
-    help="Number of angles between 0 and 2pi",
+    help="Number of angles between 0 and 2pi(evenly divided)",
     show_default=True,  # Shows the default value in help
 )
 def sin(number):
-    """Calculates sine of x over the interval from 0 to 2pi.
+    """Calculates sine of x over the interval from 0 to 2pi."""
 
-    Args:
-        number (int): Number of angles between 0 and 2pi
-    """    
-    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin(x)": np.sin(x)})
     print(df)
@@ -37,19 +33,15 @@ def sin(number):
     "--number",
     type=int,
     default=10,
-    help="Number of angles between 0 and 2pi",
+    help="Number of angles between 0 and 2pi(evenly divided)",
     show_default=True,  # Shows the default value in help
 )
 def tan(number):
-    """Calculates tangent of x over the interval from 0 to 2pi.
-
-    Args:
-        number (int): Number of angles between 0 and 2pi
-    """    
+    """Calculates tangent of x over the interval from 0 to 2pi."""
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan(x)": np.tan(x)})
     print(df)
 
 
 if __name__ == "__main__":
-    cmd_group()  # Call the command group to handle the CLI
+    cmd_group()  
